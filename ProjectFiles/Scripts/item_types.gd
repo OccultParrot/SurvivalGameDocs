c2023b237759028b4a64e368ed6f32d250ec34b4
OccultParrot <117190867+OccultParrot@users.noreply.github.com>
2023-12-06 19:04:38 -0600

    Restructured items.
    Implementation of Components
    Implementation of Tags, instead of groups

--- ProjectFiles/Scripts/item_types.gd
+++ ProjectFiles/Scripts/item_types.gd
@@ -1,41 +1,60 @@
 extends Object
+
 class_name ItemTypes
 
 """
-Item groups:
-	rock
-	air # Only holds TESTING ITEMS!!!
+Item tags:
+	- testing # When an item has the "testing" tag, debug information will be displayed about the item when hovered over
+	- weapon
 """
 
 class Item:
 	var Name : String
 	var Description : String
-	var Group : String
+	var Tags : Array
 	var Weight : float
 	var Max_Stack_Size : int
 	var Item_Texture : Texture2D
 	var Size : Vector2i
-	
-	func _init(name : String, description : String, group : String, weight : float, max_stack_size : int, item_texture : Texture2D, size : Vector2i):
+	var components : Array
+
+	func _init(name : String, description : String, tags : Array, weight : float, max_stack_size : int, item_texture : Texture2D, size : Vector2i):
 		self.Name = name
 		self.Description = description
-		self.Group = group
+		self.Tags = tags
 		self.Weight = weight
 		self.Max_Stack_Size = max_stack_size
 		self.Item_Texture = item_texture
 		self.Size = size
+		self.components = []
+
+		# Add default components based on item group
+		for tag in self.Tags:
+			match tag:
+				"weapon":
+					self.add_component(Damage.new(10))  # Default damage value is 10
+
+	func add_component(component):
+		self.components.append(component)
 
 	func _to_string():
 		return self.Name
 
-class Air extends Item:
+# Define a base class for components
+class Component:
 	func _init():
-		super._init("", "", "air", 0.0, 1, null, Vector2i(1,1))
+		pass
 
-class GraniteRock extends Item:
-	func _init():
-		super._init("Rock (Granite)", "A hard rock with speckles.", "rock", 0.2, 10, load("res://Assets/Rocks/granite_rock.png"), Vector2i(1,1))
+# Example component: Enchantable
+class Enchantable extends Component:
+	var enchant_level : int
+	
+	func _init(enchant_level : int):
+		self.enchant_level = enchant_level
 
-class GabbroRock extends Item:
-	func _init():
-		super._init("Rock (Gabbro)", "A dark coarse-grained rock.", "rock", 0.2, 10, load("res://Assets/Rocks/granite_rock.png"), Vector2i(1,1))
+# Example component: Damage
+class Damage extends Component:
+	var damage_value : int
+	
+	func _init(damage_value : int):
+		self.damage_value = damage_value
