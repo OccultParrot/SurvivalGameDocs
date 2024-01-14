9268dd7318c91cd3f337111c5454bc1794449118
OccultParrot <117190867+OccultParrot@users.noreply.github.com>
2024-01-13 21:22:01 -0600

    Updated file

--- ProjectFiles/Main.gd
+++ ProjectFiles/Main.gd
@@ -1,16 +1,6 @@
 extends Node
 
 @export var speed : float = 1
-var direction : Vector3
 
 func _process(delta):
-	direction = Vector3(0.0,0.0,0.0)
-	if Input.is_action_pressed("move_up"):
-		direction.y += 1
-	if Input.is_action_pressed("move_down"):
-		direction.y -= 1
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	$Camera3D.position += (direction.normalized() * speed) * delta
+	$Scenery/Square.rotate(Vector3(1,1,0).normalized(), 0.005)
